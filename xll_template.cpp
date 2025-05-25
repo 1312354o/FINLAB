06b269c3368ce18b851f0705c554b3a2cb11e856
1312354o <jig_u_2_mik@sjtu.edu.cn>
2025-05-25 06:26:39 -0400

    "bsm_put"

--- xll_template.cpp
+++ xll_template.cpp
@@ -33,6 +33,7 @@ double WINAPI xll_tgamma(double x)
 	return tgamma(x);
 }
 
+
 // Press Alt-F8 then type 'XLL.MACRO' to call 'xll_macro'
 // See https://xlladdins.github.io/Excel4Macros/
 AddIn xai_macro(
@@ -47,7 +48,55 @@ int WINAPI xll_macro(void)
 	// A1 style instead of default R1C1.
 	OPER reftext = Excel(xlfReftext, Excel(xlfActiveCell), OPER(true));
 	// UTF-8 strings can be used.
-	Excel(xlcAlert, OPER("XLL.MACRO called with активный 细胞: ") & reftext);
+	Excel(xlcAlert, OPER("XLL.MACRO called with : ") & reftext);
 
 	return TRUE;
+}
+
+double norm_cdf(double x)
+{
+	return 0.5 * (1 + std::erf(x / std::sqrt(2.0)));
+}
+ 
+double bsm_put(double r, double S, double sigma, double K, double t)
+{
+	if (sigma <= 0 || t <= 0 || S <= 0 || K <= 0) {
+		return std::numeric_limits<double>::quiet_NaN();
+	}
+
+	double d1 = (std::log(S / K) + (r + 0.5 * sigma * sigma) * t) / (sigma * std::sqrt(t));
+	double d2 = d1 - sigma * std::sqrt(t);
+
+	double put = K * std::exp(-r * t) * norm_cdf(-d2) - S * norm_cdf(-d1);
+
+	return put;
+}
+
+AddIn xai_bsm_put(
+	// Return double, C++ name of function, Excel name
+	Function(XLL_DOUBLE, "xll_bsm_put", "BSM.PUT")
+	// Array of function arguments
+	.Arguments({
+		Arg(XLL_DOUBLE, "r", "is the risk-free interest rate (annualized)."),
+		Arg(XLL_DOUBLE, "S", "is the current underlying asset price."),
+		Arg(XLL_DOUBLE, "sigma", "is the volatility of returns of the underlying asset (annualized)."),
+		Arg(XLL_DOUBLE, "K", "is the strike price."),
+		Arg(XLL_DOUBLE, "t", "is the time to expiration in years.")
+		})
+	// Function Wizard help
+	.FunctionHelp("Returns the Black-Scholes-Merton put option value.")
+	// Function Wizard category
+	.Category("Financial")
+	// URL linked to `Help on this function`
+	.HelpTopic("https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model")
+	.Documentation(R"xyzyx(
+The Black-Scholes-Merton put option pricing formula calculates the theoretical price of a European put option.
+)xyzyx")
+);
+
+// WINAPI calling convention must be specified
+double WINAPI xll_bsm_put(double r, double S, double sigma, double K, double t)
+{
+#pragma XLLEXPORT // must be specified to export function
+	return bsm_put(r, S, sigma, K, t);
 }
